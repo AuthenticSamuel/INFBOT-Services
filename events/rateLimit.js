1e739da9fa0a0eb862531ff39c56f858665b35f8
Samuel <samuel.ward@outlook.fr>
2021-10-11 00:52:33 +0200

    Added rate limit event

--- events/rateLimit.js
+++ events/rateLimit.js
@@ -6,7 +6,7 @@ module.exports = (rateLimitData) => {
 
     console.log(colors.red.italic(`${getDateTime()} >>> Rate limit detected:`))
     for (const [key, value] of Object.entries(rateLimitData)) {
-        console.log(colors.red.italic(`${getDateTime()} >>>`) + colors.white.italic(`  ${capitalizeFirstLetter(key)}: ${value}`));
+        console.log(colors.red.italic(`${getDateTime()} >>>`) + colors.white.italic(`   ${capitalizeFirstLetter(key)}: ${value}`));
     };
 
 };
